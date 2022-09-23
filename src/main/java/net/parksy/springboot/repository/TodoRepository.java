ba6c0d986ec2193e70961134e767e4b71f4fa530
Darryl Parks <djparks@usa.net>
2022-09-23 14:56:43 -0500

    Downgrade Spring Boot to try and get OpenAPI working...

--- src/main/java/net/parksy/springboot/repository/TodoRepository.java
+++ src/main/java/net/parksy/springboot/repository/TodoRepository.java
@@ -1,8 +1,51 @@
 package net.parksy.springboot.repository;
 
 import net.parksy.springboot.model.Todo;
-import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
-public interface TodoRepository extends ListCrudRepository<Todo,Integer> {
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class TodoRepository  {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public TodoRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    //    @Override
+    public List<Todo> findAll() {
+        return jdbcTemplate.query("SELECT * from Todo", BeanPropertyRowMapper.newInstance(Todo.class));
+    }
+
+//    @Override
+    public Optional<Todo> findById(int id) {
+        try {
+            Todo todo = jdbcTemplate.queryForObject("SELECT * FROM Todo WHERE id=?",
+                    BeanPropertyRowMapper.newInstance(Todo.class), id);
+
+            return Optional.ofNullable(todo);
+        } catch (IncorrectResultSizeDataAccessException e) {
+            return null;
+        }
+    }
+
+    public int save(Todo todo) {
+        return jdbcTemplate.update("INSERT INTO Todo (id, user_id, title, completed, version) VALUES(?,?,?,?,?)",
+                todo.id(), todo.userId(), todo.title(), todo.completed(), todo.version());
+    }
+
+    public void saveAll(List<Todo> todos) {
+        for (Todo todo: todos) {
+            save(todo);
+        }
+    }
+
 
 }
